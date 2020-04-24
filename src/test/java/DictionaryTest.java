f6b3561e556eae14dbe803fc89870cb2c01a77ec
Alexandru Tenche <alex.tenche@gmail.com>
2020-04-25 00:42:42 +0300

    new test

--- src/test/java/DictionaryTest.java
+++ src/test/java/DictionaryTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
+import ro.alex.util.DefinitionSearch;
 import ro.alex.util.Dictionary;
 
 import java.util.List;
@@ -11,13 +12,22 @@ public class DictionaryTest {
 
     @Test
     public void dictTest() {
-
         Dictionary dictionary = new Dictionary();
-
         List<String> definitions = dictionary.getDefinitions("hello");
-
         assertEquals(definitions.size(), 3);
+    }
+
+    public void mockedTest() {
+        Dictionary dictionary2 = new Dictionary(new DummyService());
 
     }
 
+
+    private class DummyService implements DefinitionSearch {
+
+        @Override
+        public List<String> getDefinition(String word) {
+            return null;
+        }
+    }
 }
