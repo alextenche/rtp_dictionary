a79bea29f920bef12bbdb6c88f3d2301e8bbdbbf
Alexandru Tenche <alex.tenche@gmail.com>
2020-04-25 00:44:33 +0300

    more dict calls

--- src/main/java/ro/alex/App.java
+++ src/main/java/ro/alex/App.java
@@ -1,6 +1,9 @@
 package ro.alex;
 
 import ro.alex.util.Dictionary;
+import ro.alex.util.HttpHelper;
+import ro.alex.util.Language;
+import ro.alex.util.WebServiceDefinitionSearch;
 
 import java.util.List;
 
@@ -11,6 +14,14 @@ public class App {
         Dictionary dictionary = new Dictionary();
         List<String> definitions = dictionary.getDefinitions("computer");
         definitions.forEach(System.out::println);
+
+        Dictionary dictionary1 = new Dictionary(new WebServiceDefinitionSearch());
+        List<String> definitions1 = dictionary.getDefinitions("plane");
+        definitions1.forEach(System.out::println);
+
+        Dictionary dictionary2 = new Dictionary(new WebServiceDefinitionSearch(new HttpHelper(), Language.SPANISH));
+        List<String> definitions2 = dictionary.getDefinitions("hola");
+        definitions2.forEach(System.out::println);
 
     }
 }
