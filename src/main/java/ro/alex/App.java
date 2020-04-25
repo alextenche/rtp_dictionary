1c3be95ac975bbf30065ca6804cc5c7d87f5cc27
Alexandru Tenche <alex.tenche@gmail.com>
2020-04-25 12:06:36 +0300

    using factories

--- src/main/java/ro/alex/App.java
+++ src/main/java/ro/alex/App.java
@@ -1,34 +1,21 @@
 package ro.alex;
 
-import ro.alex.dictionary.Dictionary;
-import ro.alex.dictionary.HttpHelper;
-import ro.alex.dictionary.Language;
-import ro.alex.dictionary.WebServiceDefinitionSearch;
+import ro.alex.dictionary.*;
+import ro.alex.dictionary.factory.DictionaryFactory;
+import ro.alex.dictionary.factory.GeneralDictionaryFactory;
 
-import java.util.List;
 
 public class App {
 
     public static void main(String[] args) {
 
-        Dictionary dictionary = new Dictionary();
-        List<String> definitions = dictionary.getDefinitions("computer");
-        definitions.forEach(System.out::println);
+        Dictionary dictionary3 = SimpleDictionaryFactory.english();
+        dictionary3.getDefinitions("computer").forEach(System.out::println);
 
-        Dictionary dictionary1 = new Dictionary(new WebServiceDefinitionSearch());
-        List<String> definitions1 = dictionary1.getDefinitions("plane");
-        definitions1.forEach(System.out::println);
+        Dictionary dictionary4 = SimpleDictionaryFactory.ofType(DictionaryType.MEDICAL);
+        dictionary4.getDefinitions("doctor").forEach(System.out::println);
 
-        Dictionary dictionary2 = new Dictionary(new WebServiceDefinitionSearch(new HttpHelper(), Language.SPANISH));
-        List<String> definitions2 = dictionary2.getDefinitions("hola");
-        definitions2.forEach(System.out::println);
-
-        Dictionary dictionary3 = Dictionary.english();
-        List<String> definitions3 = dictionary3.getDefinitions("tea");
-        definitions3.forEach(System.out::println);
-
-        Dictionary dictionary4 = Dictionary.spanish();
-        List<String> definitions4 = dictionary4.getDefinitions("hola");
-        definitions4.forEach(System.out::println);
+        DictionaryFactory dictionaryFactory = new GeneralDictionaryFactory();
+        dictionaryFactory.newDictionary(Language.ENGLISH).getDefinitions("coffee").forEach(System.out::println);
     }
 }
